2022b816d07d39caff36037133c74c25083cc0a0
zyjjyz2019 <553965263@qq.com>
2020-10-20 16:27:41 -0700

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,21 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Let's analyze some Yacht Data 🐧📊.")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    # Load the Auto MPG data from https://github.com/allisonhorst/palmerpenguins.
+    yacht_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-yet-another-streamlit/master/yacht_hydrodynamics.data"
+    return pd.read_csv(yacht_url, delim_whitespace=True, names=[
+        'buoyancy position',
+        'Prismatic coefficient',
+        'Length-displacement ratio',
+        'Beam-draught ratio',
+        'Length-beam ratio',
+        'Froude number',
+        'Residuary resistance'
+    ])
 
 df = load_data()
 
@@ -16,14 +24,37 @@ st.write("Let's look at raw data in the Pandas Data Frame.")
 
 st.write(df)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+st.write("Hmm 🤔, is there some correlation between residuary resistance and froude number? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
+pts = alt.selection(type="interval", encodings=["x"])
+
+points = alt.Chart().mark_point().encode(
+    x='Froude number',
+    y='Residuary resistance'
+).transform_filter(
+    pts
 ).properties(
-    width=600, height=400
-).interactive()
+    width=300,
+    height=300
+)
+
+mag = alt.Chart().mark_bar().encode(
+    x=alt.X('Prismatic coefficient:N', axis=alt.Axis(format='.3f')),
+    y='count()',
+    color=alt.condition(pts, alt.value("lightblue"), alt.value("lightgray"))
+).properties(
+    width=300,
+    height=300
+).add_selection(pts)
+
+chart = alt.hconcat(
+    points,
+    mag,
+    data=df
+).transform_bin(
+    "Prismatic coefficient",
+    field="Prismatic coefficient",
+    bin=alt.Bin(step=0.01)
+)
 
 st.write(chart)
